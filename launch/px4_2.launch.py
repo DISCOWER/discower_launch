45b39cd8b267d6e61b9ffeabe34b7452addb91cf
joris997 <jorisv@kth.se>
2024-11-18 11:49:20 +0000

    launch files for 1, 2, and 3 robots. still with px4_i naming conventions, so to fix!

---
+++ launch/px4_2.launch.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import os
+# from utilities.utilities import *
+
+
+def generate_launch_description():
+    """Launch Gazebo with a freeflyer running PX4 communicating over ROS 2."""
+
+    px4_dir = os.getenv("PX4_SPACE_SYSTEMS_DIR")
+    if not px4_dir:
+        raise RunTimeError("PX4_SPACE_SYSTEMS_DIR is not set. Did you add it to your .bashrc file?")
+
+    id_to_name = {"0": "snap",
+                  "1": "crackle",
+                  "2": "pop"}
+
+    return LaunchDescription(
+        [
+            # We have the first robot always start with id 0
+            DeclareLaunchArgument("id0", default_value="0"),
+            DeclareLaunchArgument("pose0", default_value="0,0,0"),
+            ExecuteProcess(
+                cmd=[
+                    "xterm",      # or "gnome-terminal", "konsole", "xterm"
+                    "-hold",      # Keep terminal open for debugging
+                    "-e",
+                    px4_dir + "/build/px4_sitl_default/bin/px4",
+                    "-i",
+                    LaunchConfiguration("id0"),
+                    "",
+
+                ],
+                cwd=px4_dir,
+                env={**os.environ,
+                    "PX4_SIM_AUTOSTART": "4001",
+                    "PX4_SIM_SPEED_FACTOR": "1",
+                    "PX4_GZ_MODEL_POSE": LaunchConfiguration("pose0"),
+                    "PX4_SIM_MODEL": "gz_spacecraft_2d"},
+                output="screen",
+            ),
+
+            DeclareLaunchArgument("id1", default_value="1"),
+            DeclareLaunchArgument("pose1", default_value="0,1,0"),
+            ExecuteProcess(
+                cmd=[
+                    "xterm",        # or "gnome-terminal", "konsole", "xterm"
+                    "-hold",      # Keep terminal open for debugging
+                    "-e",
+                    px4_dir + "/build/px4_sitl_default/bin/px4",
+                    "-i",
+                    LaunchConfiguration("id1"),
+                    "",
+
+                ],
+                cwd=px4_dir,
+                env={**os.environ,
+                    "PX4_SIM_AUTOSTART": "4001",
+                    "PX4_SIM_SPEED_FACTOR": "1",
+                    "PX4_GZ_MODEL_POSE": LaunchConfiguration("pose1"),
+                    "PX4_SIM_MODEL": "gz_spacecraft_2d"},
+                output="screen",
+            ),
+        ]
+    )
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from glob import glob
+import os
+
+package_name = 'discower_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
+    ],
+    zip_safe=True,
+    maintainer='Joris',
+    maintainer_email='jorisv@kth.se',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+)
