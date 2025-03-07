0988b1d777fbde14cae11a6474cdc65135a2aad4
joris997 <jorisv@kth.se>
2025-03-07 10:12:17 +0100

    cleaned setup.py and added license

--- setup.py
+++ setup.py
@@ -12,18 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('discower_launch/launch/*launch.py')),
-        (os.path.join('share', package_name), glob('discower_launch/config/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Joris',
     maintainer_email='jorisv@kth.se',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='simple SITL launch files for single- and multi-agent simulation of the ATMOS platform',
+    license='BSD 3-Clause',
     tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'pose_setter = discower_launch.pose_setter:main',
-        ],
-    },
+    entry_points={},
 )
