951e201475f5858310b169cfa6068d99e51eed90
joris997 <jorisv@kth.se>
2025-07-31 10:20:52 +0200

    feat: enabled bluerov sitl launch in px4.launch.py

--- discower_launch/launch/px4.launch.py
+++ discower_launch/launch/px4.launch.py
@@ -4,19 +4,32 @@ from launch.actions import DeclareLaunchArgument, OpaqueFunction, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 import os
 
+# usage:
+# ld.add_action(IncludeLaunchDescription(
+#     PythonLaunchDescriptionSource(
+#         [get_package_share_directory('discower_launch'), '/px4.launch.py']),
+#     launch_arguments={'id':'0', 'pose':'1,0,0', 'name':'snap', 'delay':'0', 'model':'uuv'}.items()
+# ))
+
 def launch_px4(context, *args, **kwargs):
     px4_dir = os.getenv("PX4_SPACE_SYSTEMS_DIR")
     if not px4_dir:
         raise RuntimeError("PX4_SPACE_SYSTEMS_DIR is not set. Did you add it to your .bashrc file?")
 
-    id_ = LaunchConfiguration("id").perform(context)
-    pose = LaunchConfiguration("pose").perform(context)
-    name = LaunchConfiguration("name").perform(context)
-    delay = LaunchConfiguration("delay").perform(context)
-    model = LaunchConfiguration("model").perform(context) if "model" in context.launch_configurations else "spacecraft_2d"
+    id_ = LaunchConfiguration("id").perform(context)        # Unique identifier for the PX4 instance (string, e.g. '0')
+    pose = LaunchConfiguration("pose").perform(context)     # Initial pose of the PX4 instance (string, e.g. "0,0,0")
+    name = LaunchConfiguration("name").perform(context)     # Name/namespace of the PX4 instance (string, e.g. "snap")
+    delay = LaunchConfiguration("delay").perform(context)   # Delay before starting the PX4 instance (string, e.g. "0")
+    model = LaunchConfiguration("model").perform(context)   # Model to use for the PX4 instance (string, e.g. "atmos" or "uuv")
     world = LaunchConfiguration("world").perform(context)
-
     model_name = f"{model}_{id_}"
+
+    target = 'px4_sitl_spacecraft'
+    if model:
+        target = 'px4_sitl_uuv' if model == "uuv" else target
+    gz_model = 'gz_spacecraft'
+    if model:
+        gz_model = 'gz_uuv_bluerov2_heavy' if model == "uuv" else gz_model
 
     use_odom_bridge = LaunchConfiguration("use_odom_bridge").perform(context).lower() == "true"
 
@@ -24,7 +37,8 @@ def launch_px4(context, *args, **kwargs):
         ExecuteProcess(
             cmd=[
                 "bash", "-c",
-                f"sleep {delay} && {px4_dir}/build/px4_sitl_default/bin/px4 -i {id_}"
+                # f"sleep {delay} && {px4_dir}/build/px4_sitl_{target}/bin/px4 -i {id_}"
+                f"sleep {delay} && {px4_dir}/build/{target}/bin/px4 -i {id_}",
             ],
             cwd=px4_dir,
             env={
@@ -34,7 +48,7 @@ def launch_px4(context, *args, **kwargs):
                 "PX4_GZ_MODEL_POSE": pose,
                 "PX4_INSTANCE": id_,
                 "PX4_DELAY": delay,
-                "PX4_SIM_MODEL": f"gz_{model}",
+                "PX4_SIM_MODEL": f"{gz_model}",
                 "PX4_UXRCE_DDS_NS": name,
                 "PX4_GZ_WORLD": world
             },
